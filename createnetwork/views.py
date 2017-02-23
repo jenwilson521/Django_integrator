100328d4ccc2de52eaf6c173c0e949d5c0020b56
Jennifer Wilson <jenwilson@DN0a22b112.SUNet>
2017-02-23 12:23:32 -0800

    Played with formatting to better display analyses

--- createnetwork/views.py
+++ createnetwork/views.py
@@ -4,4 +4,4 @@ from django.utils import timezone
 
 def analysis_list(request):
     netans = NetworkAnalysis.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'createnetwork/analysis_list.html', {})
+    return render(request, 'createnetwork/analysis_list.html', {'netans':netans})
